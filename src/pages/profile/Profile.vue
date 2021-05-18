3f6f563804c82de1a007a776f57c6107e896aeec
lishen <843567305@qq.com>
2021-05-18 12:47:52 +0800

    profile已完成

--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -1,15 +1,82 @@
 <template>
-  <div>
-    我的
+  <div id="profile">
+    <navigation-bar class="nav-bar">
+      <div slot="center">凌云阁商城</div>
+    </navigation-bar>
+    <user-info></user-info>
+    <section class="account">
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0.00</span>元
+        </div>
+        <div class="account-info">我的余额</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>个
+        </div>
+        <div class="account-info">我的优惠券</div>
+      </div>
+      <div class="account-item">
+        <div class="number">
+          <span class="balance">0</span>分
+        </div>
+        <div class="account-info">我的积分</div>
+      </div>
+    </section>
+
+    <list-view class="order-list"></list-view>
   </div>
 </template>
 
 <script>
+import UserInfo from '@/pages/profile/childComps/UserInfo'
+import ListView from '@/pages/profile/childComps/ListView'
+
+import NavigationBar from '@/components/common/navigationbar/NavigationBar'
 export default {
-  name: 'Profile'
+  name: 'Profile',
+  components: {
+    UserInfo,
+    ListView,
+    NavigationBar,
+  }
 }
 </script>
 
 <style scoped>
-
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+  font-weight: 700;
+  letter-spacing: 5px;
+}
+.account {
+  display: flex;
+}
+.account-item {
+  width: 100%;
+  background-color: #fff;
+  margin-right: 1px;
+  text-align: center;
+}
+.account-item:last-of-type {
+  margin-right: 0;
+}
+.account-item {
+  color: #666;
+  font-size: 13px;
+  padding: 18px;
+}
+.account-item .balance {
+  font-size: 24px;
+  font-weight: 700;
+  color: #ff5f3e;
+}
+.account-info {
+  margin-top: 6px;
+}
+.order-list, .service-list {
+  margin-top: 12px;
+}
 </style>
---
+++ src/pages/profile/childComps/ListView.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="list">
+      <div class="info">
+        <img src="@/assets/img/profile/message.svg" class="item-img" alt="">
+        <span>我的消息</span>
+      </div>
+      <div class="info">
+        <img src="@/assets/img/profile/shopping.svg" class="item-img" alt="">
+        <span>积分商城</span>
+      </div>
+      <div class="info">
+        <img src="@/assets/img/profile/vip.svg" class="item-img" alt="">
+        <span>会员卡</span>
+      </div>
+      <div class="info">
+        <img src="@/assets/img/profile/cart.svg" class="item-img" alt="">
+        <span>购物车</span>
+      </div>
+      <div class="info">
+        <img src="@/assets/img/profile/app.svg" class="item-img" alt="">
+        <span>下载App</span>
+      </div>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "ListView",
+    props: {
+		  listData: {
+		    type: Array,
+        default: function () {
+          return []
+        }
+      }
+    }
+ }
+</script>
+
+<style scoped>
+  #list {
+    background-color: #fff;
+    font-size: 15px;
+    color: #333;
+  }
+
+  .info {
+    height: 44px;
+    line-height: 44px;
+    margin-left: 40px;
+    border-bottom: 1px solid rgba(100, 100, 100, .1);
+    padding-left: 5px;
+  }
+  .info img {
+    width: 20px;
+    margin-right: 10px;
+    vertical-align: middle;
+  }
+  .info span {
+    vertical-align: middle;
+  }
+</style>
